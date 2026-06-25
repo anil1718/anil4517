f45bac1176c1db6212b462f4ce1b8f07583444d0
Anil kumar <anilk409390@gmail.com>
2026-06-25 22:24:04 +0530

    Create lists_practice_v2.py

---
+++ lists_practice_v2.py
@@ -0,0 +1,7 @@
+# Creating Lists
+fruits = ["apple", "banana", "mango", "orange"]
+numbers = [10, 20, 30, 40, 50]
+
+print("Fruits:", fruits)
+print("Numbers:", numbers)
+print("Length:", len(fruits))
