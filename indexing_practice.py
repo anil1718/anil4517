97e56e22876c9ddd7352d6a747577a1d967e1038
Anil kumar <anilk409390@gmail.com>
2026-06-26 07:33:02 +0530

    Create indexing_practice.py

---
+++ indexing_practice.py
@@ -0,0 +1,7 @@
+# Indexing
+fruits = ["apple", "banana", "mango", "orange"]
+numbers = [10, 20, 30, 40, 50]
+mixed = [1, "hello", 3.14, True]
+print("\n--- Indexing ---")
+print("First fruit:", fruits[0])
+print("Last fruit:", fruits[-1])
