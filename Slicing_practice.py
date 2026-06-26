ff3907c316517a0bd9913f12b5799e9b8b266945
Anil kumar <anilk409390@gmail.com>
2026-06-26 21:06:01 +0530

    Create Slicing_practice.py

---
+++ Slicing_practice.py
@@ -0,0 +1,7 @@
+# SLICING
+fruits = ["apple"," banana","mango","orange",  ]
+numbers = [1,2,3,4,5]
+mixed = ["Anil",20,8,5, True ]  # different data types
+print(fruits[1:3])  # ['banana', 'mango']
+print(fruits[:2])   # ['apple', 'banana']
+print(fruits[2:])   # ['mango', 'orange']
